1566af7fb86ab2021b46169dfaf8075f56fcfa6e
GSamar <galiya753@gmail.com>
2021-10-22 23:30:42 +0500

    infinite scrolling

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -4,15 +4,31 @@ let apiUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}&coun
 const loading = document.querySelector('.loader');
 const imageContainer = document.querySelector('.image-container');
 let photoArray = [];
+let allImagesLoaded = false;
+let loadedImages = 0;
+let totalImages = 0;
 
 function setAttributes(element, attributeName) {
   for (let key in attributeName) {
-    console.log(key, 'key from function');
+    //console.log(key, 'key from function');
     element.setAttribute(key, attributeName[key]);
   }
 }
 
+function imageLoaded() {
+  loadedImages++;
+  console.log(loadedImages);
+  if (loadedImages === totalImages) {
+    allImagesLoaded = true;
+    loading.hidden = true;
+  }
+}
+
 function displayPhoto() {
+  loadedImages = 0;
+  //totalImages += photoArray.length;
+  totalImages = photoArray.length;
+  console.log('totalImages: ', totalImages)
   photoArray.forEach((photo) => {
     //create <a>
     const linkItem = document.createElement('a');
@@ -28,7 +44,7 @@ function displayPhoto() {
       //title: photo.alt_description
     })
     //img.setAttribute('title', photo.alt_description); //No description for most photos
-
+    img.addEventListener('load', imageLoaded);
     //make structure container-a-img
     linkItem.appendChild(img);
     imageContainer.appendChild(linkItem);
@@ -45,5 +61,15 @@ async function getPhoto() {
     console.log('I caught error')
   }
 }
+
+window.addEventListener('scroll', () => {
+  if ((window.innerHeight + window.scrollY >= window.document.body.offsetHeight - window.innerHeight + 200) && (allImagesLoaded)) {
+    getPhoto();
+    allImagesLoaded = false;
+    console.log('scroll event');
+  }
+  //console.log('window.innerHeight: ', window.innerHeight);
+  //console.log('document.bady.offsetHeight: ', document.body.offsetHeight);
+});
 
 getPhoto();
